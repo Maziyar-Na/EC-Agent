348110decb6634fec1d00c8d9d43bf9734ef3b40
PreritO <PreritO@node-1.ectest.cudevopsfall2018-pg0.wisc.cloudlab.us>
2020-02-11 16:03:30 -0600

    protobuf integrated and working on cloudlab

--- Agent/Handler.cpp
+++ Agent/Handler.cpp
@@ -18,18 +18,15 @@ uint64_t ec::agent::Handler::handle_mem_req(uint64_t cgroup_id) {
 }
 
 uint64_t ec::agent::Handler::connect_container(string server_ip, string container_name) {
-    std::cout << "[dbg] In connect_container: " << std::endl;
-    std::cout << "Container name: " << container_name << std::endl;
-
+    //std::cout << "Container name: " << container_name << std::endl;
     string cmd = "sudo docker ps -a | grep k8s_" + container_name + " | awk '{print $1, $3}'";
-    std::cout << "docker cmd: " << cmd << std::endl;
-    // Puzzle here: what is the best way to wait for the container to be created? I might be sending stuff
-    // to agent too quickly here..
+    // std::cout << "docker cmd: " << cmd << std::endl;
+    
+    // Todo: Change this so that we looop for maximum of 5 seconds or until a new container is created 
     sleep(5);
+
     std::string container_id = exec(cmd);
     // This is the where we can confirm whether the container was successfully created and deployed
-    std::cout << "[should not hang]: " << std::endl;
-    std::cout << "[dbg]: " << container_id << std::endl;
     if (container_id.size() == 0) {
         std::cout << "[dbg]: No container found with name:" << container_name << std::endl;
         return (uint64_t) -1;
@@ -45,7 +42,7 @@ uint64_t ec::agent::Handler::connect_container(string server_ip, string containe
         std::cout << "[dbg]: Error in getting PID for container with name:" << container_name << std::endl;
         return (uint64_t) -1;
     }
-    cout << "[dbg] Docker Container PID: " << pid << endl;
+    // cout << "[dbg] Docker Container PID: " << pid << endl;
     pid.erase(remove(pid.begin(), pid.end(), '\n'), pid.end());
     cmd = "../../ec_syscalls/sys_connect " + server_ip + " " + pid + " 4444";
 
@@ -75,12 +72,14 @@ char* ec::agent::Handler::handle_request(char* buff){
     google::protobuf::io::CodedInputStream::Limit msgLimit = codedIn.PushLimit(siz);
     rx_msg.ParseFromCodedStream(&codedIn);
     codedIn.PopLimit(msgLimit);
-    
-    // cout<< "[RX MESSAGE DBG]: " << endl;
-    // cout<< "[ip]: " << rx_msg.client_ip() << endl;
-    // cout<< "[request type]: " << rx_msg.req_type() << endl;
-    // cout<< "[payload string]: " << rx_msg.payload_string() << endl;
-    // cout<< "[cgroup id]: " << rx_msg.cgroup_id() << endl;
+
+    /* 
+    cout<< "[RX MESSAGE DBG]: " << endl;
+    cout<< "[ip]: " << rx_msg.client_ip() << endl;
+    cout<< "[request type]: " << rx_msg.req_type() << endl;
+    cout<< "[payload string]: " << rx_msg.payload_string() << endl;
+    cout<< "[cgroup id]: " << rx_msg.cgroup_id() << endl;
+    */
 
     uint64_t ret = 0;
     switch (rx_msg.req_type() ) {
@@ -102,7 +101,6 @@ char* ec::agent::Handler::handle_request(char* buff){
             break;
         case _MEM_LIMIT_:
             ret = 2061374;//TODO: temporary. for testing purpose. we need a syscall to extract mem limit based on cgroup id
-            res->rsrc_amnt = ret;
             break;
         default:
             cerr << "[ERROR] Not going in the right way! request type is invalid!" << endl;
@@ -113,17 +111,22 @@ char* ec::agent::Handler::handle_request(char* buff){
     tx_msg.set_rsrc_amnt(ret);
     tx_msg.set_payload_string(rx_msg.payload_string());
 
-    // cout<< "[TX MESSAGE DBG]: " << endl;
-    // cout<< "[ip]: " << tx_msg.client_ip() << endl;
-    // cout<< "[rsrc amt]: " << tx_msg.rsrc_amnt() << endl;
-    // cout<< "[payload string]: " << tx_msg.payload_string() << endl;
-    
+    /*
+    cout<< "[TX MESSAGE DBG LOG] " << endl;
+    cout<< "[request type]: " << tx_msg.req_type() << endl;
+    cout<< "[rsrc amt]: " << tx_msg.rsrc_amnt() << endl;
+    cout<< "[payload string]: " << tx_msg.payload_string() << endl;
+    */
+
     int tx_size = tx_msg.ByteSizeLong()+4;
     char* tx_buf = new char[tx_size];
     google::protobuf::io::ArrayOutputStream arrayOut(tx_buf, tx_size);
     google::protobuf::io::CodedOutputStream codedOut(&arrayOut);
     codedOut.WriteVarint32(tx_msg.ByteSizeLong());
     tx_msg.SerializeToCodedStream(&codedOut);
+    
+    //std::cout << "[EC Init] Sending Message to GCM with message of length: " << tx_size << std::endl; 
+
     return tx_buf;
 }
 
@@ -131,14 +134,15 @@ void ec::agent::Handler::run(int64_t clifd) {
     char buff[__BUFFSIZE__];
     int64_t bytes_read;
     bzero(buff, __BUFFSIZE__);
-    cout << "[dbg] run: We are ready to accept request from GCM! fd is: " << clifd << endl;
+    cout << "[RUN log] We are ready to accept request from GCM! fd is: " << clifd << endl;
     char* tx_buff;
 
     while( (bytes_read = read(clifd, buff, __BUFFSIZE__) ) > 0 ) {
         tx_buff = handle_request(buff);
 
-        if (write(clifd, (char*) tx_buff, sizeof(*tx_buff)) < 0)
+        if (write(clifd, (void*) tx_buff, __BUFFSIZE__) < 0) {
             cout <<"[ERROR] writing to socket connection (Agent -> GCM) Failed! " << endl;
+        }
     }
 
     pthread_exit(NULL);
@@ -146,7 +150,7 @@ void ec::agent::Handler::run(int64_t clifd) {
 
 void* ec::agent::Handler::run_handler(void* server_args)
 {
-    cout << "[dbg] run_handler: thread executed!" << endl;
+    // cout << "[dbg] run_handler: thread executed!" << endl;
     auto *args = static_cast<serv_thread_args*>(server_args);
     args->req_handler->run(args->clifd);
     return NULL;
@@ -182,7 +186,6 @@ google::protobuf::uint32 ec::agent::Handler::readHdr(char *buf)
   google::protobuf::io::ArrayInputStream ais(buf,4);
   CodedInputStream coded_input(&ais);
   coded_input.ReadVarint32(&size);//Decode the HDR and get the size
-  cout<<" size of RX payload is "<<size<<endl;
   return size;
 }
 
