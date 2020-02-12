11677711fb73a723a15a0429423f83433abc9a41
PreritO <PreritO@node-1.ectest.cudevopsfall2018-pg0.wisc.cloudlab.us>
2020-02-11 21:41:29 -0600

    at the point where manager refactor and protobuff should be merged

--- Agent/cmake_install.cmake
+++ Agent/cmake_install.cmake
@@ -38,8 +38,9 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
+  # Include the install script for each subdirectory.
   include("/mnt/ECKernel/EC-4.20.16/EC-Agent/Agent/proto/cmake_install.cmake")
+
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
