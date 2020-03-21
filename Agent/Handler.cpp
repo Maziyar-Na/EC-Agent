5360a74193150ae0e5de4da52d9fd0b6ebd60b55
Greg Cusack <gregorycusack@gmail.com>
2020-03-20 21:58:03 -0600

    fix write back issue

--- Agent/Handler.cpp
+++ Agent/Handler.cpp
@@ -17,14 +17,34 @@ uint64_t ec::agent::Handler::handle_mem_req(uint64_t cgroup_id) {
     return avail_mem;
 }
 
-uint64_t ec::agent::Handler::handle_cpu_req(uint64_t cgroup_id, uint64_t quota) {
+uint64_t ec::agent::Handler::handle_cpu_req(uint64_t cgroup_id, uint64_t quota, uint64_t &update_quota) {
     uint64_t ret = 0;
     std::cout << "setting quota to: " << quota << std::endl;
     ret = syscall(__RESIZE_QUOTA_SYSCALL_, cgroup_id, quota / 1000); //must divide since kernel multiplies
-    if(ret) {
-        std::cout << "quota set failed" << std::endl;
+    if(ret == 1) {
+        std::cout << "quota set failed, ret: " << ret << std::endl;
+        ret = 1;
     }
+    else {
+        std::cout << "quota set success! quota set to: " << ret << std::endl;
+        update_quota = ret;
+        ret = 0;
+    }
+    return ret;
+}
 
+int64_t ec::agent::Handler::handle_read_quota(uint16_t cgroup_id) {
+    long ret = 0;
+    std::cout << "reading quota" << std::endl;
+    ret = syscall(__READ_QUOTA_SYSYCALL__, cgroup_id);
+    if(ret == -1) {
+        std::cout << "quota is RUNTIME_INF" << std::endl;
+        return -1;
+    }
+    else if(ret == 1) {
+        std::cout << "error reading quota!" << std::endl;
+        return 1;
+    }
     return ret;
 }
 
@@ -88,7 +108,8 @@ uint64_t ec::agent::Handler::connect_container(const string &server_ip, const st
 }
 
 //Helper function to handle request
-char* ec::agent::Handler::handle_request(char* buff){
+char* ec::agent::Handler::handle_request(char* buff, int &tx_size){
+
 
     google::protobuf::uint32 siz = readHdr(buff);
     msg_struct::ECMessage rx_msg;
@@ -103,13 +124,21 @@ char* ec::agent::Handler::handle_request(char* buff){
 //    std::cout<< "[request type]: " << rx_msg.req_type() <<  std::endl;
 //    std::cout<< "[cgroup id]: " << rx_msg.cgroup_id() << std::endl;
 //    std::cout << "rx_msg.quota: " << rx_msg.quota() << std::endl;
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "---------------------" << std::endl;
+    std::cout << "in quota: " << rx_msg.quota() << std::endl;
+    std::cout << "in seq num: " << rx_msg.request() << std::endl;
+//    std::cout << "in time: " << std::chrono::system_clock::to_time_t(t) << std::endl;
 
     uint64_t ret = 0;
+    uint64_t updated_quota = 0;
     std::cout << "rx_msg.req_type(): " << rx_msg.req_type() << std::endl;
     switch (rx_msg.req_type() ) {
         case _CPU_:
             std::cout << "[Agent DBG]: handle cpu_req" << std::endl;
-            ret = handle_cpu_req(rx_msg.cgroup_id(), rx_msg.quota());
+//            protolock.lock();
+            ret = handle_cpu_req(rx_msg.cgroup_id(), rx_msg.quota(), updated_quota);
+//            protolock.unlock();
             break;
         case _MEM_:
             ret = handle_mem_req(rx_msg.cgroup_id());
@@ -130,6 +159,9 @@ char* ec::agent::Handler::handle_request(char* buff){
         case _SET_MAX_MEM_:
             ret = handle_resize_max_mem(rx_msg.cgroup_id(), rx_msg.rsrc_amnt(), false);
             break;
+        case _READ_QUOTA_:
+            ret = handle_read_quota(rx_msg.cgroup_id());
+            break;
         default:
             std::cerr << "[ERROR] Not going in the right way! request type is invalid!" << std::endl;
     }
@@ -138,7 +170,16 @@ char* ec::agent::Handler::handle_request(char* buff){
     msg_struct::ECMessage tx_msg;
     tx_msg.set_req_type(rx_msg.req_type());
     tx_msg.set_rsrc_amnt(ret);
+    tx_msg.set_quota(updated_quota);
+    tx_msg.set_request(rx_msg.request());
     tx_msg.set_payload_string(rx_msg.payload_string());
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "out rsrc_amnt: " << tx_msg.rsrc_amnt() << std::endl;
+    std::cout << "out seq num: " << tx_msg.request() << std::endl;
+    std::cout << "out quota: " << tx_msg.quota() << std::endl;
+    std::cout << "duration: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << std::endl;
+
 
     /*
     cout<< "[TX MESSAGE DBG LOG] " << endl;
@@ -147,7 +188,7 @@ char* ec::agent::Handler::handle_request(char* buff){
     cout<< "[payload string]: " << tx_msg.payload_string() << endl;
     */
 
-    int tx_size = tx_msg.ByteSizeLong()+4;
+    tx_size = tx_msg.ByteSizeLong()+4;
     char* tx_buf = new char[tx_size];
     google::protobuf::io::ArrayOutputStream arrayOut(tx_buf, tx_size);
     google::protobuf::io::CodedOutputStream codedOut(&arrayOut);
@@ -155,27 +196,28 @@ char* ec::agent::Handler::handle_request(char* buff){
     tx_msg.SerializeToCodedStream(&codedOut);
     
     //std::cout << "[EC Init] Sending Message to GCM with message of length: " << tx_size << std::endl; 
-
     return tx_buf;
 }
 
 void ec::agent::Handler::run(int64_t clifd) {
-    char buff[__BUFFSIZE__];
+    char buff[__BUFFSIZE__] = {0};
     int64_t bytes_read;
-    bzero(buff, __BUFFSIZE__);
+//    bzero(buff, __BUFFSIZE__);
     std::cout << "[RUN log] We are ready to accept request from GCM! fd is: " << clifd << std::endl;
     char* tx_buff;
+    int tx_size = 0;
 
     while( (bytes_read = read(clifd, buff, __BUFFSIZE__) ) > 0 ) {
         std::cout << "rx req!" << std::endl;
-        tx_buff = handle_request(buff);
+        tx_buff = handle_request(buff, tx_size);
 //        std::cout << "handled req. tx_buff: " << tx_buff << std::endl;
 
         //TODO: temp fix. handle_request returns nullptr if it's a CPU req (on purpose)
         if(tx_buff) {
-            if (write(clifd, (void*) tx_buff, __BUFFSIZE__) < 0) {
+            if (write(clifd, (void*) tx_buff, tx_size) < 0) {
                 std::cout <<"[ERROR] writing to socket connection (Agent -> GCM) Failed! " << std::endl;
             }
+            delete[] tx_buff;
         }
         else {
             std::cout << "tx_buff == NULL" << std::endl;
@@ -226,4 +268,6 @@ google::protobuf::uint32 ec::agent::Handler::readHdr(char *buf)
   coded_input.ReadVarint32(&size);//Decode the HDR and get the size
   return size;
 }
+
+
 
--- Agent/Handler.h
+++ Agent/Handler.h
@@ -15,6 +15,9 @@
 #include <array>
 #include "om.h"
 #include "proto/msg.pb.h"
+#include <mutex>
+#include <chrono>
+#include <ctime>
 
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
@@ -27,10 +30,12 @@
 #define _CONNECT_ 4
 #define _MEM_LIMIT_ 5
 #define _SET_MAX_MEM_ 6
+#define _READ_QUOTA_ 7
 
 #define __RESIZE_MAX_MEM_SYSCALL__ 336
 #define __INCR_MEMCG_MARGIN_SYSCALL__ 337
 #define __RESIZE_QUOTA_SYSCALL_ 338
+#define __READ_QUOTA_SYSYCALL__ 339
 
 using namespace google::protobuf::io;
 
@@ -44,14 +49,17 @@ namespace ec {
             static void *run_handler(void *server_args);
 
         private:
-            char *handle_request(char *buff);
+            char *handle_request(char *buff, int &tx_size);
             static uint64_t handle_mem_req(uint64_t cgroup_id);
-            static uint64_t handle_cpu_req(uint64_t cgroup_id, uint64_t quota);
+            static uint64_t handle_cpu_req(uint64_t cgroup_id, uint64_t quota, uint64_t &updated_quota);
             static uint64_t handle_resize_max_mem(uint16_t cgroup_id, uint64_t new_limit, int is_memsw);
+
+            static int64_t handle_read_quota(uint16_t cgroup_id);
 
             static uint64_t connect_container(const std::string &server_ip, const std::string &container_name);
             static std::string exec(std::string &command);
             static google::protobuf::uint32 readHdr(char *buf);
+            std::mutex protolock;
         };
 
         struct serv_thread_args {
