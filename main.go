05083b0272fbbacc9d57f923ef62378459986b88
PreritO <PreritO@node-1.ectest.cudevopsfall2018-pg0.wisc.cloudlab.us>
2020-04-16 17:21:12 -0500

    updated main.go to be reflective (without interface) of what's in Handler.cpp - all in one file. Need to confirm it works before integrating with kubelet

--- main.go
+++ main.go
@@ -7,54 +7,187 @@ import (
 	"io"
 	"github.com/golang/protobuf/proto"
 	"msg_struct"
+	"os/exec"
+	"time"
+	"context"
+	"syscall"
+	"strconv"
+	"strings"
+	"encoding/binary"
 )
 
 const MAXGCMNO = 30
 const PORT = ":4445"
 const BUFFSIZE = 2048
+const EC_CONNECT_SYSCALL = 335
+const RESIZE_MEM_SYSCALL = 336
+const INCREASE_MEM_CG_MARGIN_SYSCALL = 337
+const RESIZE_QUOTA_SYSCALL = 338
+const READ_QUOTA_SYSCALL = 339
+
+type ProtobufData struct {
+	Size	int
+	Payload []byte
+}
+
+func ip2int(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
+
+func connect_container(server_ip, container_name string) (string, uint64) {
+	log.Printf("[DBG] CONNECT CONTAINER: server_ip %s: , container name: %s\n", server_ip, container_name)
+	
+	// Heads up, this will change once it's integrated into kubelet (cuz kubelets check for alive pods/containers in a different way)
+	cmd := "sudo docker ps -a | grep k8s_" + container_name + " | awk '{print $1}'"
+	// Loops for max of 10 seconds or whenever uit finds the container, whichever comes first
+	var container_id string
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	for {
+    	out, err := exec.CommandContext(ctx, "/bin/sh",  "-c" , cmd).Output()
+		if len(string(out)) > 0 {
+			container_id = string(out)
+			break
+		}
+		if (ctx.Err() != nil) {
+			return "Error in finding container: " + err.Error(), 1
+		}
+	}
+	container_id = strings.TrimSuffix(container_id, "\n")
+
+	cmd = "sudo docker inspect --format '{{ .State.Pid }}' " + container_id
+	out, err := exec.Command("/bin/sh", "-c", cmd).Output()  
+	if err != nil {
+		return "ERROR in getting PID of container with id " + container_id + ": " + err.Error(), 1
+	}
+	pid := string(out)
+	pid = strings.TrimSuffix(pid, "\n")
+	pid_int, err := strconv.Atoi(pid)
+    if err != nil {
+		return err.Error(), 1
+	}
+
+	// call syscall for ec_connect here
+	gcm_ip := ip2int(net.ParseIP(server_ip))
+	port := 4444
+	agentIP := ip2int(net.ParseIP("128.105.144.93"))
+	_, _, err = syscall.Syscall6(EC_CONNECT_SYSCALL, uintptr(gcm_ip) , uintptr(port), uintptr(pid_int), uintptr(agentIP) , 0, 0)
+	
+	// log.Println("Docker Container id:", container_id)
+	return container_id, 0
+}
+
+func handle_cpu_req(cgroup_id, quota uint64) (uint64, uint64) {
+	log.Printf("setting quota to: %d\n", quota)
+	quotaMega := quota/1000
+	update_quota, _, _ := syscall.Syscall(RESIZE_QUOTA_SYSCALL, uintptr(cgroup_id), uintptr(quotaMega), 0)
+	new_quota := uint64(update_quota)
+
+	if new_quota == 1 {
+		log.Printf("Quota Set Failed\n")
+		return new_quota, 1
+	} 
+
+	log.Printf("Quota Set Success!\n")
+	log.Printf("ret: %d\n", new_quota)
+	return uint64(new_quota), 0
+}
+
+func handle_mem_req(cgroup_id uint64) (uint64) {
+	log.Printf("cgroup_id: %d\n", cgroup_id)
+	avail_mem_ret, _, _ := syscall.Syscall(RESIZE_MEM_SYSCALL, uintptr(cgroup_id), 0, 0)
+	avail_mem := uint64(avail_mem_ret)
+
+	log.Printf("[INFO]: EC Agent: Reclaimed memory is: %d\n", avail_mem)
+	return avail_mem
+}
+
+func handle_resize_max_mem(cgroup_id uint64, new_limit uint64, is_memsw int) (uint64) {
+	log.Printf("setting new mem limit to: %d\n", new_limit)
+	avail_mem_ret, _, _ := syscall.Syscall(RESIZE_MEM_SYSCALL, uintptr(cgroup_id), uintptr(new_limit), uintptr(is_memsw))
+	avail_mem := uint64(avail_mem_ret)
+
+	if avail_mem == 0 {
+		log.Printf("[INFO]: EC Agent: resize_max_mem fails\n", avail_mem)
+	}
+	return avail_mem
+}
 
 func handleConnection(conn net.Conn) {
-	log.Printf("[dbg] Server: New fd created for new connection. Serving %s\n", conn.RemoteAddr().String())
+	log.Printf("[DBG] Server: New fd created for new connection. Serving %s\n", conn.RemoteAddr().String())
 	for {
 		buff := make([]byte, BUFFSIZE)
 		c := bufio.NewReader(conn)
-
+		defer conn.Close()
 		// read a single byte which contains the message length at the beginning of the message
 		size, err := c.ReadByte()
 		if err != nil {
 			log.Println("ERROR in reading Header: ", err.Error())
 		}
-		log.Println("ProtoBuf Message Body length: ", size)
-		
+		log.Println("[ProtoBuf] RX Message Body length: ", size)
 		// now, read the full Protobuf message
 		_, err = io.ReadFull(c, buff[:int(size)])
 		if err != nil {
 			log.Println("ERROR in reading Body: ", err.Error())
 		}
-
 		rxMsg := &msg_struct.ECMessage{}
 		err = proto.Unmarshal(buff[:size], rxMsg)
 		if err != nil {
 			log.Println("ERROR in ProtoBuff - UnMarshaling: ", err.Error())
 		}
-		// Debug statement
+
 		// log.Println("Recieved message req type: ", rxMsg.GetReqType())
+		var ret uint64
+		var container_id string
+		var updated_quota uint64
 		switch rxMsg.GetReqType() {
 		case 0:
 			log.Println("CPU Request")
+			updated_quota, ret = handle_cpu_req(uint64(rxMsg.GetCgroupId()), rxMsg.GetQuota())
 		case 1:
 			log.Println("Memory Request")
+			ret = handle_mem_req(uint64(rxMsg.GetCgroupId()))
 		case 2:
 			log.Println("Init Request")
 		case 3:
 			log.Println("CPU SLICE")
 		case 4: 
-			log.Println("CONNECT CONTAINER")
+			container_id, ret = connect_container(rxMsg.GetClientIp(), rxMsg.GetPayloadString())
+			if ret != 0 {
+				log.Println("[ERROR] Initial Container Connection failed...")
+				log.Println(container_id)
+			}
 		case 5:
 			log.Println("Handle RESIZE MAX/MIN")
+			ret = handle_resize_max_mem(uint64(rxMsg.GetCgroupId()), rxMsg.GetRsrcAmnt(), 0)
 		default:
 			log.Println("[ERROR] Not going in the right way! request type is invalid!")
 		}
+
+		//log.Println("Docker Container id:", container_id)
+		//log.Println("Updated Quota", updated_quota)
+		txMsg := &msg_struct.ECMessage{
+			ReqType: proto.Int32(rxMsg.GetReqType()),
+			RsrcAmnt: proto.Uint64(ret),
+			Quota: proto.Uint64(updated_quota),
+			PayloadString: proto.String(container_id),
+			Request: proto.Uint32(rxMsg.GetRequest()),
+		}
+
+		txMsgMarshal, err := proto.Marshal(txMsg)
+		if err != nil {
+			log.Fatal("TX Data marshaling error: ", err)
+		}
+
+		// Write to socket the message stream
+		length, err := conn.Write(txMsgMarshal)
+		if err != nil {
+			log.Println("[ERROR] in writing proto message to socket" + err.Error())
+		}
+		log.Printf("[PROTOBUF] TX  Message Body length: %d\n", length)
 
 	}
 }
