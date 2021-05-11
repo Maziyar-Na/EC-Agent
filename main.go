c4edf173337cfc15daab911b5aef839a9d64d380
greg <gregorycusack@gmail.com>
2021-05-10 23:19:43 -0600

    rm commented out code

--- main.go
+++ main.go
@@ -83,13 +83,11 @@ func GetDockerPid(dockerId string) (int, int, string) {
 func RunConnectContainer(gcmIpStr string, dockerId string, pid int, appNum int32) (string, int32, uint64){
 	// call syscall for ec_connect here
 	gcmIp := ip2int(net.ParseIP(gcmIpStr))
-	//port_tcp := 4444
-	//port_udp := 4447
+
 	port_tcp := BaseTcpPort + appNum - 1
 	port_udp := BaseUdpPort + appNum - 1
 	interfaceIP := getIpFromInterface(INTERFACE)
-	//log.Printf("[INFO]: IP of the interface %s is %s\n", INTERFACE, interfaceIP)
-	//agentIP := ip2int(net.ParseIP("128.105.144.93"))
+
 	fmt.Println("pid to run connectContainer: " + strconv.Itoa(pid))
 	agentIP := ip2int(interfaceIP)
 	cgId, t, err := syscall.Syscall6(EcConnectSyscall, uintptr(gcmIp) , uintptr(port_tcp), uintptr(port_udp), uintptr(pid), uintptr(agentIP), 0)
@@ -192,7 +190,6 @@ func (s *grpcControllerServer) ReqResizeMaxMem(ctx context.Context, in *pbContro
 	cgroupId := in.CgroupId
 	newLimit := in.NewMemLimit
 	isMemsw := 0
-	//isInc := 0
 
 	parentCgroupID, _, _ := syscall.Syscall(GetParentCgidSyscall, uintptr(cgroupId), 0, 0)
 	parentCgID := int32(parentCgroupID)
@@ -272,88 +269,6 @@ func (s *grpcControllerServer) ReadMemLimit(ctx context.Context, in *pbControlle
 
 }
 
-//func handleConnection(conn net.Conn) {
-//	log.Printf("[DBG] Server: New fd created for new connection. Serving %s\n", conn.RemoteAddr().String())
-//	for {
-//		buff := make([]byte, BuffSize)
-//		c := bufio.NewReader(conn)
-//		defer conn.Close()
-//		// read a single byte which contains the message length at the beginning of the message
-//		size, err := c.ReadByte()
-//		if err != nil {
-//			if err.Error() == "EOF" {
-//				log.Println("Connection killed by client")
-//				break
-//			} else {
-//				log.Println("ERROR in reading Header: ", err.Error())
-//			}
-//		}
-//		//log.Println("[ProtoBuf] RX Message Body length: ", size)
-//		// now, read the full Protobuf message
-//		_, err = io.ReadFull(c, buff[:int(size)])
-//		if err != nil {
-//			log.Println("ERROR in reading Body: ", err.Error())
-//		}
-//		rxMsg := &msg_struct.ECMessage{}
-//		err = proto.Unmarshal(buff[:size], rxMsg)
-//		if err != nil {
-//			log.Println("ERROR in ProtoBuff - UnMarshaling: ", err.Error())
-//		}
-//
-//		// log.Println("Recieved message req type: ", rxMsg.GetReqType())
-//		var ret uint64
-//		var container_id string
-//		var updated_quota uint64
-//		//log.Println("--------------- BEGIN NEW REQUEST ---------------")
-//		switch rxMsg.GetReqType() {
-//		case 0:
-//			//log.Println("CPU Request")
-//			updated_quota, ret = handleCpuReq(rxMsg.GetCgroupId(), rxMsg.GetQuota(), rxMsg.GetPayloadString())
-//		case 1:
-//			//log.Println("Memory Request")
-//			ret = handleMemReq(rxMsg.GetCgroupId())
-//		case 2:
-//			log.Println("Init Request")
-//		case 3:
-//			log.Println("CPU SLICE")
-//		case 5:
-//			//log.Println("Handle RESIZE MAX/MIN")
-//			ret = handleResizeMaxMem(rxMsg.GetCgroupId(), rxMsg.GetRsrcAmnt(), 0, 0)
-//		case 6:
-//			ret = readMemUsage(rxMsg.GetCgroupId())
-//		case 7:
-//			ret = readMemLimit(rxMsg.GetCgroupId())
-//		default:
-//			log.Println("[ERROR] Not going in the right way! request type is invalid!")
-//		}
-//		//log.Println("--------------- END NEW REQUEST ---------------")
-//
-//		//log.Println("Docker Container id:", container_id)
-//		//log.Println("Updated Quota", updated_quota)
-//		txMsg := &msg_struct.ECMessage{
-//			ReqType: rxMsg.GetReqType(),
-//			RsrcAmnt: ret,
-//			Quota: updated_quota,
-//			PayloadString: container_id,
-//			Request: rxMsg.GetRequest(),
-//		}
-//
-//		txMsgMarshal, err := proto.Marshal(txMsg)
-//		if err != nil {
-//			log.Fatal("TX Data marshaling error: ", err)
-//		}
-//
-//		// Write to socket the message stream
-//		_, err = conn.Write(txMsgMarshal)
-//		if err != nil {
-//			log.Println("[ERROR] in writing proto message to socket" + err.Error())
-//		}
-//		//log.Printf("[PROTOBUF] TX  Message Body length: %d\n", length)
-//
-//	}
-//}
-
-
 func GrpcServerDeployer(wg *sync.WaitGroup) {
 	defer wg.Done()
 	l, err := net.Listen("tcp4", PortGrpcDeployer)
@@ -384,21 +299,6 @@ func GrpcServerController(wg *sync.WaitGroup) {
 	}
 }
 
-//func TcpServer(wg *sync.WaitGroup) {
-//	defer wg.Done()
-//	l, err := net.Listen("tcp4", TcpPort)
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//	log.Println("Listening on port: " + TcpPort)
-//	for {
-//		if conn, err := l.Accept(); err == nil {
-//			go handleConnection(conn)
-//		}
-//	}
-//}
-
 func main() {
 	var wg sync.WaitGroup
 
@@ -406,6 +306,5 @@ func main() {
 
 	go GrpcServerDeployer(&wg)
 	go GrpcServerController(&wg)
-	//go TcpServer(&wg)
 	wg.Wait()
 }
