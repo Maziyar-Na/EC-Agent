9410f8597397b9876e31f2f8f005c5e6dc64da14
Greg Cusack <gregorycusack@gmail.com>
2021-05-10 23:20:58 -0600

    Ftr multitenancy (#19)
    * add in appnum
    
    * update connectcontainerrequest to contain app num
    
    * update ports with appNum from ec_deployer
    
    * fix int32 vs int issue
    
    * add grpc between controller and agent
    
    * update interface rm debug on quota set
    
    * update interface rm debug on quota set
    
    * add logging on read mem usage
    
    * add logging on read mem usage
    
    * rm commented out code

--- main.go
+++ main.go
@@ -4,15 +4,12 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/binary"
 	"fmt"
-	pb "github.com/Maziyar-Na/EC-Agent/grpc"
-	msg_struct "github.com/Maziyar-Na/EC-Agent/msg"
-	"github.com/golang/protobuf/proto"
+	pbController "github.com/Maziyar-Na/EC-Agent/containerUpdateGrpc"
+	pbDeployer "github.com/Maziyar-Na/EC-Agent/grpc"
 	"google.golang.org/grpc"
-	"io"
 	"log"
 	"net"
 	"os/exec"
@@ -22,24 +19,28 @@ import (
 	"syscall"
 )
 
-const MAXGCMNO = 30
-const TCP_PORT = ":4445"
-const PORT_GRPC	= ":4446"
-const BUFFSIZE = 2048
-const EC_CONNECT_SYSCALL = 335
-const RESIZE_MEM_SYSCALL = 336
-const INCREASE_MEM_CG_MARGIN_SYSCALL = 337
-const RESIZE_QUOTA_SYSCALL = 338
-const READ_QUOTA_SYSCALL = 339
-const GET_PARENT_CGID_SYSCALL = 340
-const READ_MEM_USAGE_SYSCALL = 341
-const READ_MEM_LIMIT_SYSCALL = 342
+//const TcpPort = ":4445"
+const PortGrpcDeployer = ":4446"
+const PortGrpcController =":4448"
+const BuffSize = 2048
+const EcConnectSyscall = 335
+const ResizeMemSyscall = 336
+const ResizeQuotaSyscall = 338
+const GetParentCgidSyscall = 340
+const BaseTcpPort = 5000
+const BaseUdpPort = 6000
+const ReadMemUsageSyscall = 341
+const ReadMemLimitSyscall = 342
 
-const INTERFACE = "enp94s0f0"
 //const INTERFACE = "enp0s3"
+const INTERFACE = "enp94s0f0"
 
-type server struct {
-	pb.UnimplementedHandlerServer
+type grpcDeployerServer struct {
+	pbDeployer.UnimplementedHandlerServer
+}
+
+type grpcControllerServer struct {
+	pbController.UnimplementedContainerUpdateHandlerServer
 }
 
 func getIpFromInterface(inter string) net.IP {
@@ -79,17 +80,17 @@ func GetDockerPid(dockerId string) (int, int, string) {
 	return pidInt, 0, ""
 }
 
-func RunConnectContainer(gcmIpStr string, dockerId string, pid int) (string, int32, uint64){
+func RunConnectContainer(gcmIpStr string, dockerId string, pid int, appNum int32) (string, int32, uint64){
 	// call syscall for ec_connect here
 	gcmIp := ip2int(net.ParseIP(gcmIpStr))
-	port_tcp := 4444
-	port_udp := 4447
+
+	port_tcp := BaseTcpPort + appNum - 1
+	port_udp := BaseUdpPort + appNum - 1
 	interfaceIP := getIpFromInterface(INTERFACE)
-	//log.Printf("[INFO]: IP of the interface %s is %s\n", INTERFACE, interfaceIP)
-	//agentIP := ip2int(net.ParseIP("128.105.144.93"))
+
 	fmt.Println("pid to run connectContainer: " + strconv.Itoa(pid))
 	agentIP := ip2int(interfaceIP)
-	cgId, t, err := syscall.Syscall6(EC_CONNECT_SYSCALL, uintptr(gcmIp) , uintptr(port_tcp), uintptr(port_udp), uintptr(pid), uintptr(agentIP), 0)
+	cgId, t, err := syscall.Syscall6(EcConnectSyscall, uintptr(gcmIp) , uintptr(port_tcp), uintptr(port_udp), uintptr(pid), uintptr(agentIP), 0)
 
 	log.Println("cgID: " + strconv.Itoa(int(cgId)) + ", t: " + string(t) + ", err: " + err.Error())
 	if int(cgId) == -1 {
@@ -99,15 +100,15 @@ func RunConnectContainer(gcmIpStr string, dockerId string, pid int) (string, int
 }
 
 // ReqContainerInfo implements agent.HandlerServer
-func (s *server) ReqConnectContainer(ctx context.Context, in *pb.ConnectContainerRequest) (*pb.ConnectContainerReply, error) {
-	log.Printf("Received: %v, %v, %v", in.GetGcmIP(), in.GetPodName(), in.GetDockerId())
+func (s *grpcDeployerServer) ReqConnectContainer(ctx context.Context, in *pbDeployer.ConnectContainerRequest) (*pbDeployer.ConnectContainerReply, error) {
+	log.Printf("Received: %v, %v, %v, %d", in.GetGcmIP(), in.GetPodName(), in.GetDockerId(), in.GetAppNum())
 	pid, ret, err := GetDockerPid(in.GetDockerId())
 	cgroupId := int32(0)
 	if ret != 0 {
 		log.Println("Error getting docker pid for container: " + in.GetDockerId() + ", Err: " + err)
 		log.Println()
 	} else {
-		_, cgId, val := RunConnectContainer(in.GcmIP, in.GetDockerId(), pid)
+		_, cgId, val := RunConnectContainer(in.GcmIP, in.GetDockerId(), pid, in.GetAppNum())
 		if val != 0 {
 			log.Println("Error getting docker pid for container: " + in.GetDockerId() + ", Err: " + string(val))
 			log.Println()
@@ -120,45 +121,51 @@ func (s *server) ReqConnectContainer(ctx context.Context, in *pb.ConnectContaine
 		}
 	}
 
-	return &pb.ConnectContainerReply{
+	return &pbDeployer.ConnectContainerReply{
 		PodName: in.GetPodName(),
 		DockerID: in.GetDockerId(),
 		CgroupID: cgroupId,
 	}, nil
 }
 
-func handleCpuReq(cgroupId int32, quota uint64, change string) (uint64, uint64) {
-	//log.Printf("setting quota to: %d\n", quota)
+func (s *grpcControllerServer) ReqQuotaUpdate(ctx context.Context, in *pbController.ContainerQuotaRequest) (*pbController.ContainerQuotaReply, error) {
+	//log.Printf("Received: %v, %v, %v, %d", in.GetCgroupId(), in.GetNewQuota(), in.GetResizeFlag(), in.GetSequenceNum())
 	var updatedQuota uint64
-	quotaMega := quota/1000
+	quotaMega := in.GetNewQuota()/1000
+	change := in.GetResizeFlag()
 	var fistCgroupToUpdate int32
 	var secondCgroupToUpdate int32
-	//var isInc int32
 
-	parentCgroupID, _, _ := syscall.Syscall(GET_PARENT_CGID_SYSCALL, uintptr(cgroupId), 0, 0)
+	parentCgroupID, _, _ := syscall.Syscall(GetParentCgidSyscall, uintptr(in.GetCgroupId()), 0, 0)
 	parentCgID := int32(parentCgroupID)
 	//log.Println("getting the parent id: ", int32(parentCgID))
 	//TODO: need error handling here
 	if change == "incr" {
 		fistCgroupToUpdate = parentCgID
-		secondCgroupToUpdate = cgroupId
+		secondCgroupToUpdate = in.GetCgroupId()
 	} else if change == "decr" {
-		fistCgroupToUpdate = cgroupId
+		fistCgroupToUpdate = in.GetCgroupId()
 		secondCgroupToUpdate = parentCgID
 	} else {
 		log.Println("[Error] change is neither incr or decr! it is: " + change)
 	}
 
 	//Which cgroup to update first is clear now -> let's do it
-	ret, _, _ := syscall.Syscall(RESIZE_QUOTA_SYSCALL, uintptr(fistCgroupToUpdate), uintptr(quotaMega), 0)
+	ret, _, _ := syscall.Syscall(ResizeQuotaSyscall, uintptr(fistCgroupToUpdate), uintptr(quotaMega), 0)
 	if ret == 1 {
 		log.Println("[Error] Quota Set Failed at the first level!")
 		ret = 1
 		updatedQuota = 0
-		return updatedQuota, uint64(ret)
+		//return updatedQuota, uint64(ret)
+		return &pbController.ContainerQuotaReply{
+			CgroupId: in.GetCgroupId(),
+			UpdateQuota: updatedQuota,
+			ErrorCode: int32(ret),
+			SequenceNum: in.GetSequenceNum(),
+		}, nil
 	}
 
-	ret, _, _ = syscall.Syscall(RESIZE_QUOTA_SYSCALL, uintptr(secondCgroupToUpdate), uintptr(quotaMega), 0)
+	ret, _, _ = syscall.Syscall(ResizeQuotaSyscall, uintptr(secondCgroupToUpdate), uintptr(quotaMega), 0)
 	if ret == 1 {
 		log.Println("Quota Set Failed at the second level!")
 		ret = 1
@@ -169,64 +176,50 @@ func handleCpuReq(cgroupId int32, quota uint64, change string) (uint64, uint64) 
 		ret = 0
 	}
 
-	return updatedQuota, uint64(ret)
+	return &pbController.ContainerQuotaReply{
+		CgroupId: in.GetCgroupId(),
+		UpdateQuota: updatedQuota,
+		ErrorCode: int32(ret),
+		SequenceNum: in.GetSequenceNum(),
+	}, nil
+
 }
 
-func handleMemReq(cgroupId int32) uint64 {
-	//log.Printf("cgroup_id: %d\n", cgroupId)
-	availMemRet, _, _ := syscall.Syscall(RESIZE_MEM_SYSCALL, uintptr(cgroupId), 0, 0)
-	availMem := uint64(availMemRet)
+func (s *grpcControllerServer) ReqResizeMaxMem(ctx context.Context, in *pbController.ResizeMaxMemRequest) (*pbController.ResizeMaxMemReply, error) {
 
-	log.Printf("[INFO]: Reclaimed memory for cgid: %d is: %d\n", cgroupId, availMem)
-	return availMem
-}
+	cgroupId := in.CgroupId
+	newLimit := in.NewMemLimit
+	isMemsw := 0
 
-func readMemUsage(cgroupId int32) uint64{
-	//log.Printf("readMemUsage(). cgroup_id: %d\n", cgroupId)
-	memUsageRet, _, _ := syscall.Syscall(READ_MEM_USAGE_SYSCALL, uintptr(cgroupId), 0, 0)
-	memUsage := uint64(memUsageRet)
-
-	log.Printf("[INFO]: Memory usage for cgid: %d is: %d\n", cgroupId, memUsage)
-	return memUsage
-}
-
-func readMemLimit(cgroupId int32) uint64{
-	//log.Printf("cgroup_id: %d\n", cgroupId)
-	memLimitRet, _, _ := syscall.Syscall(READ_MEM_LIMIT_SYSCALL, uintptr(cgroupId), 0, 0)
-	memLimit := uint64(memLimitRet)
-
-	log.Printf("[INFO]: Memory limit for cgid: %d is: %d\n", cgroupId, memLimit)
-	return memLimit
-}
-
-//Assumption: we deploy a single container per pod, when we want to resize,
-//first we change the memory limit of the pod then the target container itself
-func handleResizeMaxMem(cgroupId int32, newLimit uint64, isMemsw int, isInc int) uint64 {
-	var fistCgroupToUpdate int32
-	var secondCgroupToUpdate int32
-
-
-	parentCgroupID, _, _ := syscall.Syscall(GET_PARENT_CGID_SYSCALL, uintptr(cgroupId), 0, 0)
+	parentCgroupID, _, _ := syscall.Syscall(GetParentCgidSyscall, uintptr(cgroupId), 0, 0)
 	parentCgID := int32(parentCgroupID)
-	
-	if isInc == 1 {
-		fistCgroupToUpdate = parentCgID
-		secondCgroupToUpdate = cgroupId
-	} else {
-		fistCgroupToUpdate = cgroupId
-		secondCgroupToUpdate = parentCgID
-	}
+
+	//always decr based on setup
+	fistCgroupToUpdate := cgroupId
+	secondCgroupToUpdate := parentCgID
+
+	//if isInc == 1 {
+	//	fistCgroupToUpdate = parentCgID
+	//	secondCgroupToUpdate = cgroupId
+	//} else {
+	//	fistCgroupToUpdate = cgroupId
+	//	secondCgroupToUpdate = parentCgID
+	//}
 	//TODO: error handling needed here
 
-	errVal, _, _ := syscall.Syscall(RESIZE_MEM_SYSCALL, uintptr(fistCgroupToUpdate), uintptr(newLimit), uintptr(isMemsw))
+	errVal, _, _ := syscall.Syscall(ResizeMemSyscall, uintptr(fistCgroupToUpdate), uintptr(newLimit), uintptr(isMemsw))
 	err := uint64(errVal)
 
 	if err != 0 {
 		log.Printf("[INFO]: EC Agent: resize_max_mem fails in first level. Ret: %d \n", err)
-		return err
+		//return err
+		return &pbController.ResizeMaxMemReply{
+			CgroupId:  in.GetCgroupId(),
+			ErrorCode: int32(err),
+		}, nil
 	}
 
-	errVal, _, _ = syscall.Syscall(RESIZE_MEM_SYSCALL, uintptr(secondCgroupToUpdate), uintptr(newLimit), uintptr(isMemsw))
+	errVal, _, _ = syscall.Syscall(ResizeMemSyscall, uintptr(secondCgroupToUpdate), uintptr(newLimit), uintptr(isMemsw))
 	err = uint64(errVal)
 
 	if err != 0 {
@@ -235,117 +228,74 @@ func handleResizeMaxMem(cgroupId int32, newLimit uint64, isMemsw int, isInc int)
 		log.Printf("Successfuly resized mem for cgid %d to: %d\n", cgroupId, newLimit)
 	}
 
-	return err //err should be 0!
+	return &pbController.ResizeMaxMemReply{
+		CgroupId:  in.GetCgroupId(),
+		ErrorCode: int32(err),
+	}, nil
+
+	//return err //err should be 0!
+
 }
 
-func handleConnection(conn net.Conn) {
-	log.Printf("[DBG] Server: New fd created for new connection. Serving %s\n", conn.RemoteAddr().String())
-	for {
-		buff := make([]byte, BUFFSIZE)
-		c := bufio.NewReader(conn)
-		defer conn.Close()
-		// read a single byte which contains the message length at the beginning of the message
-		size, err := c.ReadByte()
-		if err != nil {
-			if err.Error() == "EOF" {
-				log.Println("Connection killed by client")
-				break
-			} else {
-				log.Println("ERROR in reading Header: ", err.Error())
-			}
-		}
-		//log.Println("[ProtoBuf] RX Message Body length: ", size)
-		// now, read the full Protobuf message
-		_, err = io.ReadFull(c, buff[:int(size)])
-		if err != nil {
-			log.Println("ERROR in reading Body: ", err.Error())
-		}
-		rxMsg := &msg_struct.ECMessage{}
-		err = proto.Unmarshal(buff[:size], rxMsg)
-		if err != nil {
-			log.Println("ERROR in ProtoBuff - UnMarshaling: ", err.Error())
-		}
+func (s *grpcControllerServer) ReadMemUsage(ctx context.Context, in *pbController.CgroupId) (*pbController.ReadMemUsageReply, error) {
+	log.Println("in readMemUsage")
+	memUsageRet, _, _ := syscall.Syscall(ReadMemUsageSyscall, uintptr(in.GetCgroupId()), 0, 0)
+	log.Println("here")
+	memUsage := uint64(memUsageRet)
+	log.Println("here1")
 
-		// log.Println("Recieved message req type: ", rxMsg.GetReqType())
-		var ret uint64
-		var container_id string
-		var updated_quota uint64
-		//log.Println("--------------- BEGIN NEW REQUEST ---------------")
-		switch rxMsg.GetReqType() {
-		case 0:
-			//log.Println("CPU Request")
-			updated_quota, ret = handleCpuReq(rxMsg.GetCgroupId(), rxMsg.GetQuota(), rxMsg.GetPayloadString())
-		case 1:
-			//log.Println("Memory Request")
-			ret = handleMemReq(rxMsg.GetCgroupId())
-		case 2:
-			log.Println("Init Request")
-		case 3:
-			log.Println("CPU SLICE")
-		case 5:
-			//log.Println("Handle RESIZE MAX/MIN")
-			ret = handleResizeMaxMem(rxMsg.GetCgroupId(), rxMsg.GetRsrcAmnt(), 0, 0)
-		case 6:
-			ret = readMemUsage(rxMsg.GetCgroupId())
-		case 7:
-			ret = readMemLimit(rxMsg.GetCgroupId())
-		default:
-			log.Println("[ERROR] Not going in the right way! request type is invalid!")
-		}
-		//log.Println("--------------- END NEW REQUEST ---------------")
+	log.Printf("[INFO]: Memory usage for cgid: %d is: %d\n", in.GetCgroupId(), memUsage)
 
-		//log.Println("Docker Container id:", container_id)
-		//log.Println("Updated Quota", updated_quota)
-		txMsg := &msg_struct.ECMessage{
-			ReqType: rxMsg.GetReqType(),
-			RsrcAmnt: ret,
-			Quota: updated_quota,
-			PayloadString: container_id,
-			Request: rxMsg.GetRequest(),
-		}
-
-		txMsgMarshal, err := proto.Marshal(txMsg)
-		if err != nil {
-			log.Fatal("TX Data marshaling error: ", err)
-		}
-
-		// Write to socket the message stream
-		_, err = conn.Write(txMsgMarshal)
-		if err != nil {
-			log.Println("[ERROR] in writing proto message to socket" + err.Error())
-		}
-		//log.Printf("[PROTOBUF] TX  Message Body length: %d\n", length)
-
+	k := &pbController.ReadMemUsageReply{
+		CgroupId: in.GetCgroupId(),
+		MemUsage: int64(memUsage),
 	}
+
+	log.Printf("reply msg: %ld, %d\n", (*k).MemUsage, (*k).CgroupId)
+
+	return k, nil
+
 }
 
+func (s *grpcControllerServer) ReadMemLimit(ctx context.Context, in *pbController.CgroupId) (*pbController.ReadMemLimitReply, error) {
+	memLimitRet, _, _ := syscall.Syscall(ReadMemLimitSyscall, uintptr(in.GetCgroupId()), 0, 0)
+	memLimit := uint64(memLimitRet)
 
-func GrpcServer(wg *sync.WaitGroup) {
+	log.Printf("[INFO]: Memory Limit for cgid: %d is: %d\n", in.GetCgroupId(), memLimit)
+	return &pbController.ReadMemLimitReply{
+		CgroupId: in.GetCgroupId(),
+		MemLimit: int64(memLimit),
+	}, nil
+
+}
+
+func GrpcServerDeployer(wg *sync.WaitGroup) {
 	defer wg.Done()
-	l, err := net.Listen("tcp4", PORT_GRPC)
+	l, err := net.Listen("tcp4", PortGrpcDeployer)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	s := grpc.NewServer()
-	pb.RegisterHandlerServer(s, &server{})
+	log.Println("Grpc Deployer Listening on port: " + PortGrpcDeployer)
+	pbDeployer.RegisterHandlerServer(s, &grpcDeployerServer{})
 	if err := s.Serve(l); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
-func TcpServer(wg *sync.WaitGroup) {
+func GrpcServerController(wg *sync.WaitGroup) {
 	defer wg.Done()
-	l, err := net.Listen("tcp4", TCP_PORT)
+	l, err := net.Listen("tcp4", PortGrpcController)
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	log.Println("Listening on port: " + TCP_PORT)
-	for {
-		if conn, err := l.Accept(); err == nil {
-			go handleConnection(conn)
-		}
+	s := grpc.NewServer()
+	log.Println("Grpc Controller Listening on port: " + PortGrpcController)
+	pbController.RegisterContainerUpdateHandlerServer(s, &grpcControllerServer{})
+	if err := s.Serve(l); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
@@ -354,7 +304,7 @@ func main() {
 
 	wg.Add(2)
 
-	go GrpcServer(&wg)
-	go TcpServer(&wg)
+	go GrpcServerDeployer(&wg)
+	go GrpcServerController(&wg)
 	wg.Wait()
 }
